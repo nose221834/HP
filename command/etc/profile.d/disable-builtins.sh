2d0b14efbb89aa03c158b2c4e54cdf2c18e9693b
nose221834 <iatyy01151821@gmail.com>
2025-05-08 19:42:44 +0900

    feat: 不許可にするコマンドを増やす

--- command/etc/profile.d/disable-builtins.sh
+++ command/etc/profile.d/disable-builtins.sh
@@ -1,20 +1,64 @@
-enable -n help
-enable -n echo
-enable -n jobs
-enable -n kill
-enable -n logout
-enable -n set
-enable -n suspend
-enable -n type
-enable -n umask
-enable -n wait
-enable -n unalias
+enable -n .
+enable -n :
+enable -n [
+# enable -n alias
+enable -n bg
 enable -n bind
+enable -n break
 enable -n builtin
+enable -n caller
+enable -n cd
 enable -n command
+enable -n compgen
+enable -n complete
+enable -n compopt
+enable -n continue
 enable -n declare
 enable -n dirs
 enable -n disown
+enable -n echo
+enable -n eval
+enable -n exec
+# enable -n exit
+# enable -n export
+enable -n false
+enable -n fc
+enable -n fg
+enable -n getopts
+enable -n hash
+enable -n help
+enable -n history
+enable -n jobs
+enable -n kill
+enable -n let
+enable -n local
+enable -n logout
+enable -n mapfile
+enable -n popd
+enable -n printf
+enable -n pushd
+# enable -n pwd
+enable -n read
+enable -n readarray
+enable -n readonly
+enable -n return
+enable -n set
+enable -n shift
+enable -n shopt
+enable -n source
+enable -n suspend
+enable -n test
+# enable -n times
+enable -n trap
+enable -n true
+enable -n type
+enable -n typeset
+enable -n ulimit
+enable -n umask
+enable -n unalias
+enable -n wait
 
 # 必ず最後に実行する
-enable -n enable
+# enable -n enable
+
+# 許可してあるコマンド：exit,times,pwd,exoprt,alias
