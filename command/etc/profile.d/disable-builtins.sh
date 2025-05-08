33f7b2c25ac2237c3e33bff144b90dbab73b067e
nose221834 <iatyy01151821@gmail.com>
2025-05-08 19:05:58 +0900

    feat: 使用させたくないbuiltinコマンドを無効化

---
+++ command/etc/profile.d/disable-builtins.sh
@@ -0,0 +1,20 @@
+enable -n help
+enable -n echo
+enable -n jobs
+enable -n kill
+enable -n logout
+enable -n set
+enable -n suspend
+enable -n type
+enable -n umask
+enable -n wait
+enable -n unalias
+enable -n bind
+enable -n builtin
+enable -n command
+enable -n declare
+enable -n dirs
+enable -n disown
+
+# 必ず最後に実行する
+enable -n enable
