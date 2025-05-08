42cc62ff0d03ca509f9b80a12c14a3d4d6be0ddc
nose221834 <iatyy01151821@gmail.com>
2025-05-08 09:30:59 +0900

    feat: helpの返り値を用途通りに

--- command/help/main.go
+++ command/help/main.go
@@ -5,5 +5,11 @@ import (
 )
 
 func main() {
-    fmt.Println("コマンドが不正です。`-help` フラグを使用して使い方を確認してください。")
+    fmt.Println("[Command Help] This is a command help message.")
+	fmt.Println("Usage: command [options]")
+	fmt.Println("Options:")
+	fmt.Println("  help     この画面を表示します")
+	fmt.Println("  profile  プロファイルを表示します")
+	fmt.Println("  cat      ファイルを表示します && 猫が出てきます")
+	fmt.Println("  sl       slが出てきます")
 }
