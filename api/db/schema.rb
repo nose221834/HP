ebf0f9a8b23f58664a6198056f3f3e2b453f7aee
nose221834 <iatyy01151821@gmail.com>
2025-05-15 18:57:26 +0900

    run: bin/rails db:prepare

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_15_071330) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_15_085814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,5 +19,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_15_071330) do
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "bio", null: false
   end
 end
