13ed827c9b76ac000115743e893a4dd4427abfc6
ichinose takumi <115196617+nose221834@users.noreply.github.com>
2025-07-06 20:05:49 +0900

    Revert "Railsのコードをリファクタ"

--- api/app/channels/application_cable/connection.rb
+++ api/app/channels/application_cable/connection.rb
@@ -2,7 +2,6 @@ module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :connection_identifier
 
-    # websocket通信を一意ごとにわけるために生成している
     def connect
       self.connection_identifier = SecureRandom.uuid
     end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -11,9 +11,7 @@ Rails.application.routes.draw do
   namespace "api" do
     namespace "v1" do
       # websocketを使用するためのルーティング
-      namespace "ws" do
-        mount ActionCable.server => "/terminal"
-      end
+      mount ActionCable.server => "/cable"
     end
   end
 end
