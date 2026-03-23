cfc65d0b2cd5685eed971f76c3bb2e86533b229e
nose221834 <iatyy01151821@gmail.com>
2026-03-23 18:06:12 +0900

    wsを採用し、go一本にするためRailsとRedisを削除

--- api/README.md
+++
@@ -1,5 +0,0 @@
-## 以下のコマンドで作成した
-
-```
-rails new api --api --database=postgresql --skip-solid --skip-action-mailbox   --skip-action-text --skip-active-storage --skip-kamal
-```
--- api/app/channels/application_cable/connection.rb
+++
@@ -1,9 +0,0 @@
-module ApplicationCable
-  class Connection < ActionCable::Connection::Base
-    identified_by :connection_identifier
-
-    def connect
-      self.connection_identifier = SecureRandom.uuid
-    end
-  end
-end
